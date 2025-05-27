1c5872a9d2cf3a04059da0677e349429af489053
aishah Ali <ayoshali13@gmail.com>
2025-05-27 11:09:24 +0300

    add all

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
     <> 
-    <nav className='bg-blue-900 text-white uppercase flex flex-row items-center justify-center'>
+    <nav className='bg-blue-900 text-white uppercase flex flex-row items-center justify-center mb-2'>
         <ul className='flex flex-row items-center w-full gap-3 p-4'>
             <li>home</li>
             <li>about</li>
